d7df5019d0a99cbab2b42a559ee4ad5fa259cac3
tomusdrw <tomusdrw@users.noreply.github.com>
2020-01-13 13:10:55 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.4d94f90587b78f9e336682cc00a314b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0097955fb898a5e89406cdfa23d4f6da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5694b8887f3652d8322b",
+    "url": "/static/css/main.148e0aa3.chunk.css"
+  },
+  {
+    "revision": "dea127504fd5b927b095",
+    "url": "/static/js/2.e1762512.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e1762512.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5694b8887f3652d8322b",
+    "url": "/static/js/main.2e9020df.chunk.js"
+  },
+  {
+    "revision": "4475aafb5fb3cd8ea7fa",
+    "url": "/static/js/runtime-main.bbd065c3.js"
+  }
+]);
